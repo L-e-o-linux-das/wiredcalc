36d422d0aaf7496ab329e4d1e00209d9f1d3e936
leo mux <68807718+L-e-o-linux-das@users.noreply.github.com>
2022-03-30 21:54:01 +0600

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,27 +1,25 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
 setup(
-  name = 'wiredcalculator',         
-  packages = ['wiredcalculator'],   
-  version = '0.1',      
-  license='MIT',        
-  description = 'lol this is a calculator',   
-  author = 'leomux',                  
-  author_email = 'leo.das.mux@gmail.com',      
-  url = 'https://github.com/user/reponame',   
-  download_url = '',  
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   
-  install_requires=[           
-          'validators',
-          'beautifulsoup4',
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',      
-    'Intended Audience :: Developers',      
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   
-    'Programming Language :: Python :: 3',      
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-  ],
+  name='wiredcalc',
+  version='0.0.2',
+  description='an weird calulator',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url = 'https://github.com/L-e-o-linux-das/wiredcalc/',
+  download_url = 'https://github.com/L-e-o-linux-das/wiredcalc/archive/refs/tags/V_02.tar.gz',
+  author='Leo Mux',
+  author_email='leo.das.nirjhar@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='calc', 
+  packages=find_packages(),
+  install_requires=[''] 
 )
